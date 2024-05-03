355cdcba8df5c37b975bc52ec563ee629980667b
Wuid0801 <pig10326@naver.com>
2024-05-04 01:54:12 +0900

    [FEAT] 콜백 함수에 대한 연습 2024.05.04 1. 기본적인 setTimeout을 이용해 콜백함수를 연습했습니다.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+// 콜백 함수
+// 내생각 - 어떤 함수에서 다른 함수의 매개변수로 전달하여 비동기 처리를 하는 함수
+
+// StackOverflow의 한 이용자의 답변
+// 1. 다른 함수의 인자로써 이용되는 함수.
+// 2. 어떤 이벤트에 의해 호출되어지는 함수.
+
 
 function App() {
+
+  function CallbackData(callback) {
+    setTimeout(() => {
+      const data = "비동기 데이터";
+      // 콜백 함수 호출
+      callback(data);
+    }, 2000);
+  }
+  // 콜백 함수 정의
+  function handleData(data) {
+    console.log("데이터를 받았습니다:", data);
+  }
+
+  // CallbackData 함수 호출하면서 콜백 함수 전달
+  CallbackData(handleData);
+
+  console.log("CallbackData 함수 호출됨. 비동기 처리 중...");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
